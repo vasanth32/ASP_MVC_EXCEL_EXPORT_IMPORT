0e233bb111e08ec44f19e33609d426b2efdd2b46
vasanth <vasanthhari321@gmail.com>
2022-08-20 14:44:18 +0530

    First Commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Web;
+
+using System.Web.Mvc;
+
+using System.Data;
+
+using System.Data.SqlClient;
+
+using System.Configuration;
+
+using System.Data.OleDb;
+
+using System.IO;
+
+namespace Excel_MVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString);
+
+        OleDbConnection Econ;
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+
+        public ActionResult Index(HttpPostedFileBase file)
+
+        {
+
+            string filename = Guid.NewGuid() + Path.GetExtension(file.FileName);
+
+            string filepath = "/excelfolder/" + filename;
+
+            file.SaveAs(Path.Combine(Server.MapPath("/excelfolder"), filename));
+
+            InsertExceldata(filepath, filename);
+
+
+
+            return View();
+
+        }
+
+
+
+        private void ExcelConn(string filepath)
+
+        {
+
+            string constr = string.Format(@"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={0};Extended Properties=""Excel 12.0 Xml;HDR=YES;""", filepath);
+
+            Econ = new OleDbConnection(constr);
+
+
+
+        }
+
+
+
+        private void InsertExceldata(string fileepath, string filename)
+
+        {
+
+            string fullpath = Server.MapPath("/excelfolder/") + filename;
+
+            ExcelConn(fullpath);
+
+            string query = string.Format("Select * from [{0}]", "Sheet1$");
+
+            OleDbCommand Ecom = new OleDbCommand(query, Econ);
+
+            Econ.Open();
+
+
+
+            DataSet ds = new DataSet();
+
+            OleDbDataAdapter oda = new OleDbDataAdapter(query, Econ);
+
+            Econ.Close();
+
+            oda.Fill(ds);
+
+
+
+            DataTable dt = ds.Tables[0];
+
+
+
+            SqlBulkCopy objbulk = new SqlBulkCopy(con);
+
+            objbulk.DestinationTableName = "tbl_registration";
+
+            objbulk.ColumnMappings.Add("Email", "Email");
+
+            objbulk.ColumnMappings.Add("Password", "Password");
+
+            objbulk.ColumnMappings.Add("Name", "Name");
+
+            objbulk.ColumnMappings.Add("Address", "Address");
+
+            objbulk.ColumnMappings.Add("City", "City");
+
+
+            con.Open();
+
+            objbulk.WriteToServer(dt);
+
+            con.Close();
+
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
